bd09de01498be0c901f923bad860dcc872b32f1d
Vbrawl <greninjagr@gmail.com>
2023-09-05 13:41:13 +0300

    Added example of using the BurgerButton.

---
+++ examples/BurgerButton.php
@@ -0,0 +1,18 @@
+<?php
+chdir('..');
+require_once('main.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php \SMART_OBJECTS\LoadBurgerButton(); ?>
+</head>
+<body>
+    <smartobj-burger-button></smartobj-burger-button>
+</body>
+</html>
