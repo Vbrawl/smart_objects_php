e4e9a2e55fa74bb99a7d99ee6cfd9c5cd9824212
Vbrawl <greninjagr@gmail.com>
2023-09-07 21:51:16 +0300

    Added js relocation.

--- examples/FileSelector.php
+++ examples/FileSelector.php
@@ -9,6 +9,7 @@ require_once('main.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="/relocation.js"></script>
     <?php SMART_OBJECTS\loadFileSelector(); ?>
 </head>
 <body>
