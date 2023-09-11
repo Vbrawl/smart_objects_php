c22aa189eae11b5850bd6f0099d5c1d9de466f21
Vbrawl <greninjagr@gmail.com>
2023-09-11 22:06:27 +0300

    Moved from MutationObserver to life-cycle callbacks.

--- src/FileSelector/FileSelector.js
+++ src/FileSelector/FileSelector.js
@@ -9,105 +9,87 @@
 
     var file_id = 0;
 
-
-
-    function label_toggle(observations) {
-        for (let i = 0; i < observations.length; i++) {
-            const observation = observations[i];
-            const target = observation.target;
-            const label = target.getElementsByClassName("label")[0];
-
-            if(observation.type === 'childList') {
-                if(target.children.length > 2) {
-                    if(label) label.setAttribute('hidden', true);
-                }
-                else {
-                    if(label) label.removeAttribute('hidden');
-                    else smart_objects.FileSelector.add_label_placeholder(target);
-                }
-            }
-            else if(observation.type === 'attributes') {
-                if(label) label.innerText = target.getAttribute('label') || '';
-                else smart_objects.FileSelector.add_label_placeholder(target);
-            }
-        }
-    }
-
-    const observer = new MutationObserver(label_toggle);
-
     smart_objects.FileSelector = class extends HTMLElement {
         constructor() {
             super();
 
-            smart_objects.FileSelector.add_label_placeholder(this);
-            smart_objects.FileSelector.add_input_object(this);
+            this.init = false;
+        }
 
-            this.addEventListener('nds-point', (e) => {
-                if(e.target === this || e.target.classList.contains('label') || e.target.classList.contains('image_box')) {
-                    var input = this.getElementsByTagName('input')[0];
-                    if(!input) {
-                        input = smart_objects.FileSelector.add_input_object(this);
+        connectedCallback() {
+            if(this.isConnected && !this.init) {
+                this.init = true;
+
+                this.input = document.createElement('input');
+                this.input.setAttribute('type', 'file');
+                this.input.setAttribute('multiple', true);
+                this.input.setAttribute('hidden', true);
+
+                this.input.addEventListener('change', (e) => {
+                    e.preventDefault();
+                    const filelist = e.target.files;
+                    e.target.value = ''; // clear filelist
+
+                    for(let i = 0; i < filelist.length; i++) {
+                        const file = filelist[i];
+                        this.add_file(file);
                     }
-                    input.click();
-                }
-            });
-
-            this.addEventListener('dragover', (e) => {e.preventDefault();});
-
-            this.addEventListener('dragenter', (e) => {
-                e.preventDefault();
-                this.classList.add("hovering");
-            });
-
-            this.addEventListener('dragleave', (e) => {
-                e.preventDefault();
-                const rect = this.getBoundingClientRect();
-                if(e.pageX <= rect.left || e.pageX >= rect.right || e.pageY <= rect.top || e.pageY >= rect.bottom) {
-                    this.classList.remove("hovering");
-                }
-            });
-
-            this.addEventListener('drop', (e) => {
-                e.preventDefault();
-                this.classList.remove('hovering');
-
-                for (let i = 0; i < e.dataTransfer.files.length; i++) {
-                    const file = e.dataTransfer.files[i];
-                    this.add_file(file);
-                }
-            });
+                });
+                this.appendChild(this.input);
 
 
-            observer.observe(this, {childList: true, attributes: true});
+                this.label = document.createElement('div');
+                this.label.classList.add('label');
+                this.label.innerText = this.getAttribute('label') || '';
+                this.appendChild(this.label);
+
+
+
+                this.addEventListener('nds-point', (e) => {
+                        this.input.click();
+                });
+
+                this.addEventListener('dragover', (e) => {e.preventDefault();});
+
+                this.addEventListener('dragenter', (e) => {
+                    e.preventDefault();
+                    this.classList.add("hovering");
+                });
+
+                this.addEventListener('dragleave', (e) => {
+                    e.preventDefault();
+                    const rect = this.getBoundingClientRect();
+                    if(e.pageX <= rect.left || e.pageX >= rect.right || e.pageY <= rect.top || e.pageY >= rect.bottom) {
+                        this.classList.remove("hovering");
+                    }
+                });
+
+                this.addEventListener('drop', (e) => {
+                    e.preventDefault();
+                    this.classList.remove('hovering');
+
+                    for (let i = 0; i < e.dataTransfer.files.length; i++) {
+                        const file = e.dataTransfer.files[i];
+                        this.add_file(file);
+                    }
+                });
+            }
         }
 
-        static add_label_placeholder(_this) {
-            const label = document.createElement('div');
-            label.classList.add('label');
-            label.innerText = _this.getAttribute('label') || '';
-            _this.appendChild(label);
-            return label;
+        attributeChangedCallback(name, oldValue, newValue) {
+            switch (name) {
+                case 'label':
+                    if(this.label)
+                        this.label.innerText = newValue;
+                    break;
+            
+                default:
+                    break;
+            }
         }
 
-        static add_input_object(_this) {
-            const input = document.createElement('input');
-            input.setAttribute('type', 'file');
-            input.setAttribute('multiple', true);
-            input.setAttribute('hidden', true);
-
-            input.addEventListener('change', (e) => {
-                e.preventDefault();
-                const filelist = e.target.files;
-                e.target.value = ''; // clear filelist
-
-                for(let i = 0; i < filelist.length; i++) {
-                    const file = filelist[i];
-                    _this.add_file(file);
-                }
-            });
-
-            _this.appendChild(input);
-            return input;
+        static get observedAttributes() {
+            return ['label'];
         }
 
         add_file(file) {
@@ -137,10 +119,13 @@
             imgcontainer.classList.add('image_container');
 
             imgcontainer.addEventListener('nds-point', (e) => {
+                e.stopPropagation();
                 const on_removefile = new CustomEvent('file-selector__removefile', {detail: {file_id: fileid}, bubbles: true, cancelable: true});
 
                 if(this.dispatchEvent(on_removefile)) {
                     imgbox.remove();
+                    if(this.children.length <= 2)
+                        this.label.removeAttribute('hidden');
                 }
             });
             img.src = source;
@@ -148,8 +133,10 @@
             imgbox.appendChild(imgcontainer);
             imgcontainer.appendChild(img);
             this.appendChild(imgbox);
+            this.label.setAttribute('hidden', '');
 
             const on_thumbnail_loaded = new CustomEvent('file-selector__thumbnail_loaded', {detail: {dom_element: imgbox, file_id: fileid}, bubbles: true});
+            this.dispatchEvent(on_thumbnail_loaded);
         }
     }
 
--- src/SortableList/SortableList.js
+++ src/SortableList/SortableList.js
@@ -10,71 +10,69 @@
     smart_objects.loaded_objects = smart_objects.loaded_objects || [];
     smart_objects.loaded_objects.push("SortableList");
 
-    function make_draggable(node, x_padding, y_padding) {
-        if(!node.classList.contains("drag-n-drop__placeholder") && node.getAttribute('smartobj-sortable-list__item-initialized') !== 'true' && node.getAttribute('smartobj-sortable-list__draggable') !== "false") {
-            node.setAttribute('drag-n-drop', 'draggable');
-            node.setAttribute('drag-n-drop-x-padding', x_padding);
-            node.setAttribute('drag-n-drop-y-padding', y_padding);
-            node.setAttribute('smartobj-sortable-list__item-initialized', true);
-            node.addEventListener('drag-n-drop__drag-enter', modify_sorting);
-        }
-    }
-
-    function reindex(node) {
-        for (let i = 1; i <= node.children.length; i++) {
-            const child = node.children[i-1];
-            child.setAttribute('smartobj-sortable-list__item-index', i);
-        }
-    }
-
-    function modify_sorting(evt) {
-        const targetIndex = parseInt(evt.target.getAttribute('smartobj-sortable-list__item-index'));
-        const lastIndex = evt.target.parentElement.children.length;
-        const draggableIndex = parseInt(evt.detail.original_position.getAttribute('smartobj-sortable-list__item-index'));
-        var before_child = evt.target;
-
-        if(targetIndex > draggableIndex) {
-            if(targetIndex === lastIndex) {
-                before_child = null;
-            }
-            else {
-                before_child = evt.target.parentElement.querySelector(`*[smartobj-sortable-list__item-index="${targetIndex+1}"]`);
-            }
-        }
-        evt.target.parentElement.insertBefore(evt.detail.original_position, before_child);
-    }
-
     smart_objects.SortableList = class extends HTMLElement {
         constructor() {
             super();
 
-            this.setAttribute('drag-n-drop', "container");
+            this.init = false;
+        }
 
-            if(this.getAttribute('x-padding') === null)
-                this.setAttribute('x-padding', 0);
+        connectedCallback() {
+            if(this.isConnected && !this.init) {
+                this.init = true;
 
-            if(this.getAttribute('y-padding') === null)
-                this.setAttribute('y-padding', 0);
+                this.setAttribute('drag-n-drop', "container");
+
+                if(this.getAttribute('x-padding') === null)
+                    this.setAttribute('x-padding', 0);
+
+                if(this.getAttribute('y-padding') === null)
+                    this.setAttribute('y-padding', 0);
 
             
-            for (let i = 0; i < this.children.length; i++)
-                make_draggable(this.children[i], this.getAttribute('x-padding'), this.getAttribute('y-padding'));
+                for (let i = 0; i < this.children.length; i++)
+                    this.make_draggable(this.children[i]);
 
-            reindex(this);
+                this.reindex();
+            }
+        }
 
-            observer.observe(this, {childList: true});
+        reindex() {
+            for (let i = 1; i <= this.children.length; i++) {
+                const child = this.children[i-1];
+                child.setAttribute('smartobj-sortable-list__item-index', i);
+            }
+        }
+
+        make_draggable(node) {
+            if(!node.classList.contains("drag-n-drop__placeholder") && node.getAttribute('smartobj-sortable-list__item-initialized') !== 'true' && node.getAttribute('smartobj-sortable-list__draggable') !== "false") {
+                node.setAttribute('drag-n-drop', 'draggable');
+                node.setAttribute('drag-n-drop-x-padding', this.getAttribute('x-padding'));
+                node.setAttribute('drag-n-drop-y-padding', this.getAttribute('y-padding'));
+                node.setAttribute('smartobj-sortable-list__item-initialized', true);
+                node.addEventListener('drag-n-drop__drag-enter', smart_objects.SortableList.modify_sorting);
+            }
+        }
+
+        static modify_sorting(evt) {
+            const targetIndex = parseInt(evt.target.getAttribute('smartobj-sortable-list__item-index'));
+            const lastIndex = evt.target.parentElement.children.length;
+            const draggableIndex = parseInt(evt.detail.object.children[0].getAttribute('smartobj-sortable-list__item-index'));
+            var before_child = evt.target;
+
+            if(targetIndex > draggableIndex) {
+                if(targetIndex === lastIndex) {
+                    before_child = null;
+                }
+                else {
+                    before_child = evt.target.parentElement.querySelector(`*[smartobj-sortable-list__item-index="${targetIndex+1}"]`);
+                }
+            }
+            evt.target.parentElement.insertBefore(evt.detail.original_position, before_child);
+            smart_objects.SortableList.prototype.reindex.call(evt.target.parentElement);
+            evt.detail.object.children[0].setAttribute('smartobj-sortable-list__item-index', evt.detail.original_position.getAttribute('smartobj-sortable-list__item-index'));
         }
     }
 
-
-    var observer = new MutationObserver((mutations) => {
-        for (let i = 0; i < mutations.length; i++) {
-            const mutationRecord = mutations[i];
-            for (let i = 0; i < mutationRecord.addedNodes.length; i++) {
-                make_draggable(mutationRecord.addedNodes[i], mutationRecord.target.getAttribute('x-padding'), mutationRecord.target.getAttribute('y-padding'))
-            }
-            reindex(mutationRecord.target);
-        }
-    });
     customElements.define("smartobj-sortable-list", smart_objects.SortableList);
 }(window.smart_objects = window.smart_objects || {}));
