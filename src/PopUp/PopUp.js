41212d26137b288860706ce577d1a6064d2dbfa6
Vbrawl <greninjagr@gmail.com>
2023-09-15 15:40:42 +0300

    Reverted back to each object having it's nds-point event listener.

--- src/PopUp/PopUp.js
+++ src/PopUp/PopUp.js
@@ -25,6 +25,13 @@
                 container.addEventListener("nds-point", (evt) => {
                     evt.stopPropagation();
                 });
+
+                this.addEventListener("nds-point", (evt) => {
+                    const event = new CustomEvent('smartobj-popup-closed', {cancelable: true, bubbles: true});
+                    if(this.dispatchEvent(event)) {
+                        this.close_popup();
+                    }
+                });
                 
                 this.appendChild(container);
             }
@@ -41,15 +48,5 @@
 
     customElements.define('smartobj-popup', smart_objects.PopUp);
 
-    document.addEventListener("nds-point", (evt) => {
-        const popups = document.getElementsByTagName('smartobj-popup');
-        for (let i = 0; i < popups.length; i++) {
-            const popup = popups[i];
-            const event = new CustomEvent('smartobj-popup-closed', {cancelable: true, bubbles: true});
-            if(popup.dispatchEvent(event)) {
-                popup.close_popup();
-            }
-        }
-    });
 
 }(window.smart_objects = window.smart_objects || {}))
